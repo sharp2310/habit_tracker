ca5aa7b900f367f43bf9ed5f68f557960780a30b
sharp2310 <ilyamalahov038@gmail.com>
2024-07-25 11:40:06 +0300

    Initial commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,117 @@
-"""
-WSGI config for config project.
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+class Migration(migrations.Migration):
+    initial = True
 
-import os
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-application = get_wsgi_application()
+    operations = [
+        migrations.CreateModel(
+            name="Habit",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "place",
+                    models.CharField(
+                        max_length=150,
+                        verbose_name="Место привычки"),
+                ),
+                ("time", models.TimeField(
+                    verbose_name="Время выполнения привычки")
+                 ),
+                (
+                    "action",
+                    models.TextField(
+                        max_length=300,
+                        verbose_name="Действие, которое следует выполнять",
+                    ),
+                ),
+                (
+                    "is_pleasant",
+                    models.BooleanField(
+                        blank=True,
+                        null=True,
+                        verbose_name="Признак приятной привычки"
+                    ),
+                ),
+                (
+                    "periodicity",
+                    models.CharField(
+                        choices=[
+                            ("Раз в день", "Раз в день"),
+                            ("Раз в два дня", "Раз в два дня"),
+                            ("Раз в три дня", "Раз в три дня"),
+                            ("Раз в четыре дня", "Раз в четыре дня"),
+                            ("Раз в пять дней", "Раз в пять дней"),
+                            ("Раз в шесть дней", "Раз в шесть дней"),
+                            ("Раз в неделю", "Раз в неделю"),
+                        ],
+                        default="Раз в день",
+                        verbose_name="Периодичность",
+                    ),
+                ),
+                (
+                    "reward",
+                    models.CharField(
+                        blank=True,
+                        max_length=200,
+                        null=True,
+                        verbose_name="Награда за выполнение",
+                    ),
+                ),
+                (
+                    "duration",
+                    models.DurationField(
+                        blank=True,
+                        default=None,
+                        null=True,
+                        verbose_name="Продолжительность выполнения (в сек)",
+                    ),
+                ),
+                (
+                    "is_public",
+                    models.BooleanField(
+                        blank=True,
+                        default=False,
+                        null=True,
+                        verbose_name="Признак публичности",
+                    ),
+                ),
+                (
+                    "relate_habit",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="habits.habit",
+                        verbose_name="Связанная привычка",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="Создатель привычки",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "Привычка",
+                "verbose_name_plural": "Привычки",
+            },
+        ),
+    ]
--- habits/management/commands/bot.py
+++
@@ -1,9 +0,0 @@
-from django.core.management import BaseCommand
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **kwargs):
-        from habits.telegram import bot
-
-        bot()
